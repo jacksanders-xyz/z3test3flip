fe7ec2c72741750ca04f5e37bc2e1c17e526b05f
Jack Sanders <Guitarbreakerjs@gmail.com>
2020-12-24 15:35:15 -0700

    initial skateboard stuff

--- App.js
+++ App.js
@@ -18,23 +18,14 @@ import {
 } from 'react-native';
 
 import {
-  ViroVRSceneNavigator,
-  ViroARSceneNavigator
+  ViroARSceneNavigator,
 } from 'react-viro';
 
-/*
- TODO: Insert your API key below
- */
-var sharedProps = {
-  apiKey:"API_KEY_HERE",
-}
 
 // Sets the default scene you want for AR and VR
 var InitialARScene = require('./js/HelloWorldSceneAR');
-var InitialVRScene = require('./js/HelloWorldScene');
 
 var UNSET = "UNSET";
-var VR_NAVIGATOR_TYPE = "VR";
 var AR_NAVIGATOR_TYPE = "AR";
 
 // This determines which type of experience to launch in, or UNSET, if the user should
@@ -47,35 +38,31 @@ export default class ViroSample extends Component {
 
     this.state = {
       navigatorType : defaultNavigatorType,
-      sharedProps : sharedProps
     }
     this._getExperienceSelector = this._getExperienceSelector.bind(this);
     this._getARNavigator = this._getARNavigator.bind(this);
-    this._getVRNavigator = this._getVRNavigator.bind(this);
     this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
     this._exitViro = this._exitViro.bind(this);
   }
 
-  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-  // if you are building a specific type of experience.
+  // The top level switch, that says "has a button been pressed? which one?" based on the
+  // state of navigatorType
   render() {
     if (this.state.navigatorType == UNSET) {
       return this._getExperienceSelector();
-    } else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
-      return this._getVRNavigator();
     } else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
       return this._getARNavigator();
     }
   }
 
-  // Presents the user with a choice of an AR or VR experience
+  // THE MAIN MENU, (returns js for the main menu) 
   _getExperienceSelector() {
     return (
       <View style={localStyles.outer} >
         <View style={localStyles.inner} >
 
           <Text style={localStyles.titleText}>
-            Choose your desired experience:
+            Welcome to FLIPPLY, pick a trick:
           </Text>
 
           <TouchableHighlight style={localStyles.buttons}
@@ -85,12 +72,6 @@ export default class ViroSample extends Component {
             <Text style={localStyles.buttonText}>AR</Text>
           </TouchableHighlight>
 
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>VR</Text>
-          </TouchableHighlight>
         </View>
       </View>
     );
@@ -104,16 +85,10 @@ export default class ViroSample extends Component {
     );
   }
   
-  // Returns the ViroSceneNavigator which will start the VR experience
-  _getVRNavigator() {
-    return (
-      <ViroVRSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
-    );
-  }
 
   // This function returns an anonymous/lambda function to be used
   // by the experience selector buttons
+
   _getExperienceButtonOnPress(navigatorType) {
     return () => {
       this.setState({
