7d6262f8ec667229302c5e38d11979e49c49e801
Jack Sanders <Guitarbreakerjs@gmail.com>
2020-12-23 17:56:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App.js';
+
+AppRegistry.registerComponent('frontend_flipply', () => App);
+
+// The below line is necessary for use with the TestBed App
+AppRegistry.registerComponent('ViroSample', () => App);
